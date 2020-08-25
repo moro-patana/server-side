2c3eb2567fc5603ff1da953af9bc853c588839bc
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-08-25 11:25:55 +0300

    put the nunjucks after I instantiate the app

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-
+const nunjucks = require('nunjucks');
 // We need Express to act as our application server
 const express = require('express');
 
 // Let's create a new App
 const app = express();
+nunjucks.configure({
+  autoescape: true,
+  express: app
+});
+
 
 // We want express to run on this port
 const port = 3000;
 
 app.get('/', (request, response) => {
-  console.log('Request: /');
-  response.sendFile(__dirname + '/index.html');
+  response.render('./views/index.html.njk');
 });
 
 app.listen(port, () => {
